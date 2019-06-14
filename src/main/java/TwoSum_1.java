beb479a8b7a6841c8b0a9232ff33a66f13fcba4c
rqxiao <1007689622@qq.com>
2019-06-14 14:08:58 +0800

    TwoSum

--- src/main/java/TwoSum_1.java
+++ src/main/java/TwoSum_1.java
@@ -38,8 +38,11 @@ public class TwoSum_1 {
     }
 
 
-    /*
-     *利用hashmap存储数组，map.containsKey方法时间复杂度是O(1)，所以是O(n)
+    /*两遍哈希表
+     *
+     *利用hashmap存储数组，map.containsKey方法时间复杂度是O(1)，所以总体的时间复杂度是O(n)
+     *通过以空间换取速度的方式，我们可以将查找时间从 O(n) 降低到 O(1)。哈希表正是为此目的而构建的，它支持以 近似 恒定的时间进行快速查找。我用“近似”来描述，是因为一旦出现冲突，查找用时可能会退化到 O(n)。但只要你仔细地挑选哈希函数，在哈希表中进行查找的用时应当被摊销为 O(1)。
+     *至于hashmap查找一个元素为什么是O(1)，接近O(1),或者map.containsKey方法为什么要这么写(n - 1) & hash 可以参考https://blog.csdn.net/a2615381/article/details/78151061
      */
     public int[] twoSum2(int[] nums, int target) {
         int[] result=new int[2];
@@ -60,11 +63,27 @@ public class TwoSum_1 {
     }
 
 
+
+    public int[] twoSum3(int[] nums, int target) {
+        int[] result=new int[2];
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for (int i = 0; i <nums.length ; i++) {
+            int element=target-nums[i];
+            if(map.containsKey(element)){
+                result[0]= i;
+                result[1]= map.get(element);
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         int[ ] nums={2, 7, 11, 15};
         int target=9;
         TwoSum_1 twoSum_1=new TwoSum_1();
-        int[] result = twoSum_1.twoSum2(nums, 9);
+        int[] result = twoSum_1.twoSum3(nums, 9);
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
