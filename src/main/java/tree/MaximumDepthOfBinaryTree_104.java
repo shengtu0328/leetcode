2548e7acfdc61489ae4d52d107831a1b70eb0403
rqxiao <1007689622@qq.com>
2019-07-04 14:38:30 +0800

    0

--- src/main/java/tree/MaximumDepthOfBinaryTree_104.java
+++ src/main/java/tree/MaximumDepthOfBinaryTree_104.java
@@ -19,6 +19,8 @@ package tree;
  *     /  \
  *    15   7             第三层
  *
+ *                       null层
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
