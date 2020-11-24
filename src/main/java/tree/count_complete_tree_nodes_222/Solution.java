1addeaba0940db33417a5ce4215c0dbbf00595d3
Administrator <1007689622@qq.com>
2020-11-24 18:10:42 +0800

    count_complete_tree_nodes_222

---
+++ src/main/java/tree/count_complete_tree_nodes_222/Solution.java
@@ -0,0 +1,85 @@
+package tree.count_complete_tree_nodes_222;
+
+import tree.TreeNode;
+
+/**
+ * @author ：rqxiao
+ * @description：TODO
+ * @date ：2020/11/24 17:04
+ * <p>
+ * 给出一个完全二叉树，求出该树的节点个数。
+ * <p>
+ * 说明：
+ * <p>
+ * 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * 1
+ * / \
+ * 2   3
+ * / \  /
+ * 4  5 6
+ * <p>
+ * 输出: 6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-complete-tree-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 如果二叉树中除去最后一层节点为满二叉树，且最后一层的结点依次从左到右分布，则此二叉树被称为完全二叉树。
+ */
+public class Solution {
+
+
+    /**
+     * 什么是满二叉树？ 如果二叉树中除去最后一层节点为满二叉树，且最后一层的结点依次从左到右分布，则此二叉树被称为完全二叉树。
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countLevel(root.left);
+        int right = countLevel(root.right);
+        if (left == right) {
+            return countNodes(root.right) + (1 << left);
+        } else {
+            return countNodes(root.left) + (1 << right);
+        }
+    }
+
+    private int countLevel(TreeNode root) {
+        int level = 0;
+        while (root != null) {
+            level++;
+            root = root.left;
+        }
+        return level;
+    }
+
+
+    public static void main(String[] args) {
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+
+        t1.left = t2;
+        t1.right = t3;
+
+        t2.left = t4;
+        t2.right = t5;
+
+        t3.left = t6;
+
+        Solution solution = new Solution();
+
+        solution.countNodes(t1);
+    }
+
+
+}
